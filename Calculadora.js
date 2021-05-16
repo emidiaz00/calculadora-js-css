9097f9ebbdd5b0d34761aef96f6481080b17e8f5
emidiaz00 <79696588+emidiaz00@users.noreply.github.com>
2021-05-16 19:57:12 -0300

    Add files via upload

---
+++ Calculadora.js
@@ -0,0 +1,15 @@
+class Calculadora {
+    multiplicar(num1, num2) {
+        return num1 * num2;
+    }
+    restar(num1,num2) {
+        return num1-num2;
+    }
+    sumar(num1,num2) {
+        return num1 + num2;
+    }
+    dividir( num1,num2 ) {
+        return num1 / num2;
+    }
+
+}
